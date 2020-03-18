e02ac019807cbf612ff64231f83aea76fe74bef2
Brynjar Ingimarsson <brynjar@ingimarsson.is>
2020-03-18 22:23:13 +0000

    added command line arguments

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
 # can-mqtt-bridge
 
-This program listens for packets on a CAN bus interface and relays them to an MQTT broker. The CAN messages and signals must be specified in a DBC file along with the MQTT topic for each signal.
+**Note:** this program is incomplete.
+
+This program listens for messages on a CAN bus interface, decodes them according to a DBC file and forwards them to an MQTT broker. The DBC file must include an MQTT topic in the comment field of each signal.
+
+The program is written for a real-time telemetry system for a racecar, but can surely be used in many other scenarios.
 
 ## Installation
 
+First you will need to install the following dependencies.
+
+ - dbcppp
+ - paho-mqtt
+
+Now you can compile the program.
+
     $ g++ can-mqtt.cpp -o can-mqtt -ldbcppp
 
-Note that this program is incomplete.
+## Usage
+
+Below are the command line options for this program.
+
+    Usage: can-mqtt-bridge [options] <CAN interface>
+
+    Options: -d FILE          DBC file to decode CAN frames
+             -f FREQUENCY     Maximum frequency to forward to MQTT for each signal
+             -v               Verbose mode, prints all decoded values
+             -H               MQTT host to publish to
+             -U               MQTT username
+             -P               MQTT password
+             -p               MQTT port
+             -h               Display this help message
+
+    Example: can-mqtt-bridge -d can.dbc -f 5 -H mqtt.example.com -U admin -P admin -p 1883 can0
+
+
--- can-mqtt.cpp
+++ can-mqtt.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <iomanip>
 #include <string.h>
 #include <unistd.h>
 
@@ -15,13 +17,86 @@
 
 using namespace dbcppp;
 
+void print_usage() {
+    std::cout << "can-mqtt-bridge: Decode and forward CAN bus signals to an MQTT broker.\n" << std::endl;
+    std::cout << "Usage: can-mqtt-bridge [options] <CAN interface>\n" << std::endl;
+    std::cout << "Options: -d FILE          DBC file to decode CAN frames" << std::endl;
+    std::cout << "         -f FREQUENCY     Maximum frequency to forward to MQTT for each signal" << std::endl;
+    std::cout << "         -v               Verbose mode, prints all decoded values" << std::endl;
+    std::cout << "         -H               MQTT host to publish to" << std::endl;
+    std::cout << "         -U               MQTT username" << std::endl;
+    std::cout << "         -P               MQTT password" << std::endl;
+    std::cout << "         -p               MQTT port" << std::endl;
+    std::cout << "         -h               Display this help message\n" << std::endl;
+    std::cout << "Example: can-mqtt-bridge -d can.dbc -f 5 -H mqtt.example.com -U admin -P admin -p 1883 can0\n" << std::endl;
+}
+
+void log(std::string msg) {
+    std::time_t t = std::time(nullptr);
+    std::tm tm = *std::localtime(&t);
+
+    std::cout << '[' << std::put_time(&tm, "%F %T") << ']';
+    std::cout << ' ' << msg << std::endl;
+}
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "invalid usage" << std::endl;
-        return 0;
+    int opt;
+    int frequency = 0;
+    int verbose = 0;
+
+    std::string dbc_path = "can.dbc";
+    std::string mqtt_host = "";
+    std::string mqtt_username = "";
+    std::string mqtt_password = "";
+    int mqtt_port = 1883;
+
+    if (argc < 2) {
+        print_usage();
+        return 1;
     }
 
-    std::string iface = argv[1];
+    while ((opt = getopt(argc, argv, "d:f:vH:U:P:p:h")) != -1) {
+        switch (opt) {
+            case 'd':
+                dbc_path = optarg;
+                break;
+
+            case 'f':
+                frequency = atoi(optarg);
+                break;
+            
+            case 'v':
+                verbose = 1;
+                break;
+ 
+            case 'H':
+                mqtt_host = optarg;
+                break;
+ 
+            case 'U':
+                mqtt_username = optarg;
+                break;
+ 
+            case 'P':
+                mqtt_password = optarg;
+                break;
+ 
+            case 'p':
+                mqtt_port = atoi(optarg);
+                break;
+ 
+            case 'h':
+                print_usage();
+                exit(1);
+                break;
+        }
+    }
+
+    std::string iface = argv[argc-1];
+
+    log("using DBC file " + dbc_path);
+    log("using frequency " + std::to_string(frequency) + " Hz");
+    log("using MQTT host " + mqtt_host + ":" + std::to_string(mqtt_port));
 
     int s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
 
@@ -40,24 +115,20 @@ int main(int argc, char **argv) {
     auto net = Network::fromDBC(dbc_file);
 
     if (bind(s, (struct sockaddr *)&addr, sizeof(addr))) {
-        std::cout << "bind error" << std::endl;
-        return 0;
+        log("bind error");
+        return 1;
     }
 
     while (1) {
         nbytes = read(s, &frame, sizeof(struct can_frame));
 
-        std::cout << frame.can_id << std::endl;
-        std::cout << std::hex << frame.data[0] + 0x00 << std::endl;
-
         const Message* msg = net->getMessageById(frame.can_id);
         
         if (msg) {
-            std::cout << "Received message: " << msg->getName() << std::endl;
             (*msg).forEachSignal(
                 [&](const Signal& signal) {
                     double raw = signal.decode(frame.data);
-                    std::cout << "\t" << signal.getName() << "=" << signal.rawToPhys(raw) << std::endl;
+                    log(signal.getComment() + " " + std::to_string(signal.rawToPhys(raw)));
                 });
         }
     }
